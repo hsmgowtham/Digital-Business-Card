44afe16649612963461e780ae67d820590308a14
hsmgowtham <hsmgowtham@gmail.com>
2024-05-20 13:25:20 -0400

    Update Image

--- digital-business-card/src/components/Main.js
+++ digital-business-card/src/components/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 import profile_pic from '../profile.png';
 
 
-export default function Main(){
+export default function Main() {
     return (
         <main>
             <div id='hero-img'>
@@ -12,10 +12,21 @@ export default function Main(){
             <div className='card-content'>
                 <h4>Hi, I'm Gowtham Hanumanthu</h4>
                 <p>
-               I'm a Software and Data Engineer pursuing a Master's in Computer Science. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
+                    I'm a Software and Data Engineer pursuing a Master's in Computer Science. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
                 </p>
+                <div className='contact'>
+                    <i class="fa-brands fa-linkedin-in"></i> 
+                    <a href='https://www.linkedin.com/in/hsmgowtham'>linkedin.com/hsmgowtham</a><br/>
+
+                    <i class="fa-brands fa-github"></i>
+                    <a href='https://www.github.com/hsmgowtham'>github.com/hsmgowtham</a><br/>
+
+                    <i class="fa-regular fa-envelope"></i>
+                    <a href='mailto:gowthamhsm@gmail.com'>gowthamhsm@gmail.com</a><br/>
+                </div>
+
 
             </div>
-        </main>     
+        </main>
     )
 }
