306bc49f27e534e3abda85ac5b32a6c9f7b5d7e9
hsmgowtham <hsmgowtham@gmail.com>
2024-05-20 13:51:03 -0400

    Update format

--- digital-business-card/src/components/Main.js
+++ digital-business-card/src/components/Main.js
@@ -10,19 +10,29 @@ export default function Main() {
                 <img src={profile_pic} alt='Profile' className='profile-pic' />
             </div>
             <div className='card-content'>
-                <h4>Hi, I'm Gowtham Hanumanthu</h4>
+                <h1>Hi, I'm Gowtham Hanumanthu</h1>
                 <p>
-                    I'm a Software and Data Engineer pursuing a Master's in Computer Science. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
+                    I'm a Software and Data Engineer pursuing a Master's in Computer Science at the University of Cincinnati. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
                 </p>
+                <br/>
+                <br/>
                 <div className='contact'>
+                    <i class="fa-regular fa-envelope"></i>
+                    <a href='mailto:gowthamhsm@gmail.com'>gowthamhsm@gmail.com</a><br/><br/>
+
+                    <i class="fa-solid fa-phone"></i>
+                    <a href='tel:+15134305254'>513-430-5254</a><br/><br/>
+
                     <i class="fa-brands fa-linkedin-in"></i> 
-                    <a href='https://www.linkedin.com/in/hsmgowtham'>linkedin.com/hsmgowtham</a><br/>
+                    <a href='https://www.linkedin.com/in/hsmgowtham'>linkedin.com/hsmgowtham</a><br/><br/>
 
                     <i class="fa-brands fa-github"></i>
-                    <a href='https://www.github.com/hsmgowtham'>github.com/hsmgowtham</a><br/>
+                    <a href='https://www.github.com/hsmgowtham'>github.com/hsmgowtham</a><br/><br/>
 
-                    <i class="fa-regular fa-envelope"></i>
-                    <a href='mailto:gowthamhsm@gmail.com'>gowthamhsm@gmail.com</a><br/>
+                    <i class="fa-solid fa-location-dot"></i>
+                    <a href='https://maps.app.goo.gl/o7pVTZndpMLb12P69'>Cincinnati, Ohio, The USA</a><br/><br/>
+
+                    
                 </div>
 
 
