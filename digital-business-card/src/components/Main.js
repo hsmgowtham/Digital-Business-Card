de7d944f1d289bd7e48359000f8b4406be98f310
hsmgowtham <hsmgowtham@gmail.com>
2024-05-20 14:16:59 -0400

    Update changes

--- digital-business-card/src/components/Main.js
+++ digital-business-card/src/components/Main.js
@@ -14,25 +14,25 @@ export default function Main() {
                 <p>
                     I'm a Software and Data Engineer pursuing a Master's in Computer Science at the University of Cincinnati. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
                 </p>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <div className='contact'>
                     <i class="fa-regular fa-envelope"></i>
-                    <a href='mailto:gowthamhsm@gmail.com'>gowthamhsm@gmail.com</a><br/><br/>
+                    <a href='mailto:gowthamhsm@gmail.com'>gowthamhsm@gmail.com</a><br /><br />
 
                     <i class="fa-solid fa-phone"></i>
-                    <a href='tel:+15134305254'>513-430-5254</a><br/><br/>
+                    <a href='tel:+15134305254'>513-430-5254</a><br /><br />
 
-                    <i class="fa-brands fa-linkedin-in"></i> 
-                    <a href='https://www.linkedin.com/in/hsmgowtham'>linkedin.com/hsmgowtham</a><br/><br/>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                    <a href='https://www.linkedin.com/in/hsmgowtham'>linkedin.com/hsmgowtham</a><br /><br />
 
                     <i class="fa-brands fa-github"></i>
-                    <a href='https://www.github.com/hsmgowtham'>github.com/hsmgowtham</a><br/><br/>
+                    <a href='https://www.github.com/hsmgowtham'>github.com/hsmgowtham</a><br /><br />
 
                     <i class="fa-solid fa-location-dot"></i>
-                    <a href='https://maps.app.goo.gl/o7pVTZndpMLb12P69'>Cincinnati, Ohio, The USA</a><br/><br/>
+                    <a href='https://maps.app.goo.gl/o7pVTZndpMLb12P69'>Cincinnati, Ohio, The USA</a><br /><br />
 
-                    
+
                 </div>
 
 
