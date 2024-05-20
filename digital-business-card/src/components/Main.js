0cbe616f72af76a70c99245583992ca10d043f1a
hsmgowtham <hsmgowtham@gmail.com>
2024-05-20 15:05:32 -0400

    Update new look v2

--- digital-business-card/src/components/Main.js
+++ digital-business-card/src/components/Main.js
@@ -10,21 +10,28 @@ export default function Main() {
                 <img src={profile_pic} alt='Profile' className='profile-pic' />
             </div>
             <div className='card-content'>
-                <h1>Hi, I'm Gowtham Hanumanthu</h1>
-                <p>
-                    I'm a Software and Data Engineer pursuing a Master's in Computer Science at the University of Cincinnati. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
-                </p>
-                <br />
-                <br />
-                <div className='contact'>
-                    <i class="fa-regular fa-envelope"></i>
-                    <a href='mailto:gowthamhsm@gmail.com'>gowthamhsm@gmail.com</a><br /><br />
+                <h1>Gowtham Hanumanthu</h1>
+                <h3>Software and Data Engineer</h3>
 
+                <div className='contact'>
+
+                    <div className='contact-buttons'>
+
+                        <a href='mailto:gowthamhsm@gmail.com' className='metallic-button'><i class="fa-regular fa-envelope"></i>Email</a>
+
+
+                        <a href='https://www.linkedin.com/in/hsmgowtham' className='metallic-button'><i class="fa-brands fa-linkedin-in"></i>LinkedIn</a>
+
+                    </div>
+                    <h3>About</h3>
+                    <p>
+                        Experienced Software Engineer currently pursuing a Master's in Computer Science at the University of Cincinnati. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
+                    </p>
+                    <br />
                     <i class="fa-solid fa-phone"></i>
                     <a href='tel:+15134305254'>513-430-5254</a><br /><br />
 
-                    <i class="fa-brands fa-linkedin-in"></i>
-                    <a href='https://www.linkedin.com/in/hsmgowtham'>linkedin.com/hsmgowtham</a><br /><br />
+
 
                     <i class="fa-brands fa-github"></i>
                     <a href='https://www.github.com/hsmgowtham'>github.com/hsmgowtham</a><br /><br />
