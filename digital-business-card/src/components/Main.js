37ec59b0900bfc8c19c5135e0a485c0433126486
hsmgowtham <hsmgowtham@gmail.com>
2024-05-20 13:09:44 -0400

    Update Image

--- digital-business-card/src/components/Main.js
+++ digital-business-card/src/components/Main.js
@@ -9,6 +9,13 @@ export default function Main(){
             <div id='hero-img'>
                 <img src={profile_pic} alt='Profile' className='profile-pic' />
             </div>
+            <div className='card-content'>
+                <h4>Hi, I'm Gowtham Hanumanthu</h4>
+                <p>
+               I'm a Software and Data Engineer pursuing a Master's in Computer Science. I love exploring awesome new tools and technologies that amaze me. I stay busy tinkering with tech to keep boredom at bay. Nothing excites me more than getting hands-on with new development and design projects.
+                </p>
+
+            </div>
         </main>     
     )
 }
